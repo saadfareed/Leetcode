10709552e495ce05354e16ff9fc2b7819a297aa5
saadfareed <2018CS632@student.uet.edu.pk>
2022-06-15 23:55:49 +0500

    reverseint

--- reverseint.py
+++ reverseint.py
@@ -1,4 +1,9 @@
-num=10012
+num=-12
+flag=True
+if(num<0):
+    flag=False
+    num=-num
+#print(num)
 l=len(str(num))
 z=l
 answer=0
@@ -12,5 +17,6 @@ for i in range(0,l):
     powa=10**z
     final=(result*(powa))/(10**(i))
     answer=final+answer
-
+if(flag==False):
+    answer=-answer
 print(int(answer))
