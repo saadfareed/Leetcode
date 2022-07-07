d60ea4c28843548cea470e5e17e5bf9d7e9279bb
Sadi <50300882+saadfareed@users.noreply.github.com>
2022-07-07 00:34:00 -0700

    Problem#1

--- twoSum.py
+++ twoSum.py
@@ -4,3 +4,4 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return([i, j])
+Solution.twoSum(Solution, [2, 7, 11, 15], 9)
