fe7004a30f124abc1dcfbcaf5fd29d8344563c31
ch-ayesha <108192526+ch-ayesha@users.noreply.github.com>
2022-10-23 21:44:29 +0500

    Valid Prenthesis (#96)
    * hacktoberfest contribution
    
    * Hacktoberfest Contribution
    
    * Hactoberfest Contributions
    
    * Hacktoberfest Contributions

---
+++ Python/valid parenthesis.py
@@ -0,0 +1,31 @@
+# Name: Ayesha Aslam
+
+# Username: ch-ayesha
+
+# Approach: Solve this problem using stack and its time complexity is O(n)
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        pairs = {')' : '(', '}' : '{', ']' : '['}
+        inputs = ['(', '{', '[']
+        stack = []
+        for char in s:
+            if char in inputs:
+                stack.append(char)
+            else:
+                check = pairs[char]
+    
+                if len(stack) == 0 or check != stack[-1]:
+                    return False
+                else:
+                    stack.pop()
+        
+        if len(stack) == 0:        
+            return True
+        return False
+if __name__ == '__main__':
+    s = Solution()
+    print (s.isValid( "()"))
