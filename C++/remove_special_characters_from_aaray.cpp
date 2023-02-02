357a04d464bf47a127aa1bc3af51953671ee3faf
Shoaib19 <shoaibsabir019@gmail.com>
2023-02-02 23:37:40 +0500

    removing specials characters from array problem still in progress

--- C++/remove_special_characters_from_aaray.cpp
+++ C++/remove_special_characters_from_aaray.cpp
@@ -3,6 +3,31 @@
 //  Approach: 
 class Solution {
 public:
-    void remove_special_charactes(int arr [],int size) {
-  }
+    int remove_character(char arr [], char ch, int size){
+    int i;
+    for(i=0; i< size; i++)
+    {
+        if(arr[i] == ch)
+        break;
+    }
+    if(i < size)
+    {
+        size -= 1;
+        for(int j=i; j < size; j++)
+        {
+            arr[j] = arr[j + 1];
+        }
+        arr[size + 1] = 0;
+    }
+    return size;
+}
+
+void remove_special_charactes(char arr [],int size) {
+    
+    for(int i=0; i< size; i++)
+    {
+        if(arr[i] < 97 && arr[i] < 122)
+        size = remove_character(arr,arr[i],size);
+    }
+}
 };
