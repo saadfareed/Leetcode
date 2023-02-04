5195391748d711dbdf2efda66e109b552fa23a1f
Shoaib19 <shoaibsabir019@gmail.com>
2023-02-05 00:43:39 +0500

    started problem to write algo in n time complexcity max frquency of char

--- Ruby/count_max_letter_in_array.rb
+++ Ruby/count_max_letter_in_array.rb
@@ -3,6 +3,9 @@
 # Name: Shoaib Sabir
 # Username: Shoaib019
 # Approach:
-def find_max_frequency_character(arr)
-
+def find_max_frequency_character(str)
+  arr = [nil,nil]
+  str.each_char_with_index do |ch,idx|
+      
+  end
 end
