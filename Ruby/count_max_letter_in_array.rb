f0cc457c6464b4ff22065071b7c202846a6e2c9d
Shoaib19 <shoaibsabir019@gmail.com>
2023-02-07 22:42:45 +0500

    finally done max frequncy in string in n time complexcity

--- Ruby/count_max_letter_in_array.rb
+++ Ruby/count_max_letter_in_array.rb
@@ -2,11 +2,13 @@
 
 # Name: Shoaib Sabir
 # Username: Shoaib019
-# Approach:
+# Approach: take hash and initialize it with zero run loop over string
+#           increase count every time in hash key which is character
+#           then use max_by built in method to find letter with max count
 def find_max_frequency_character(str)
-  arr = [nil, nil]
-  str.each_char_with_index do |ch, idx|
-    # do from here
-   
+  hash = Hash.new(0)
+  str.each_char do |ch|
+    hash[ch] += 1
   end
+  hash.max_by { |_, v| v }.first
 end
