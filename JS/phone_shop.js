5469c661594e097448f6114101613abcbcb67a7a
Shoaib19 <shoaibsabir019@gmail.com>
2023-02-09 23:17:00 +0500

    js phone shop template

---
+++ JS/phone_shop.js
@@ -0,0 +1,17 @@
+TAX_RATE = 
+PHONE_PRICE = 
+ACCESSORY_PRICE = 
+SPENDING_THRESHOLD = 
+var bank_account_balance =
+
+function calculateTotalPrice() {
+
+}
+
+function calculatePurchaseAmount() {
+
+}
+
+function calculatingTax(){
+  
+}
