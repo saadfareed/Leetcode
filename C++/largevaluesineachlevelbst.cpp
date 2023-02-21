380be1e8e3d7770ee395086f8864f93b16ac809e
cinderallaman1 <ahmaddddddddddkhan@gmail.com>
2023-02-22 00:26:11 +0500

    Largestvalue in each level BST

---
+++ C++/largevaluesineachlevelbst.cpp
@@ -0,0 +1,31 @@
+class Solution{
+    public:
+    vector<int>largestvalueineachlevel(TreeNode*root){
+        if(root == NULL)
+            return {};
+        vector<int>max_vals;
+        int size;
+        int max;
+        TreeNode*temp;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            size = q.size();
+            max = INT_MIN;
+            while(size--){
+                temp = q.front();
+                q.pop();
+                max = max(max, temp->val);
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right){
+                    q.push(temp->right);
+                }
+            max_vals.push_back(max);
+            }
+        }
+        return max_vals;
+
+    }
+
+};
