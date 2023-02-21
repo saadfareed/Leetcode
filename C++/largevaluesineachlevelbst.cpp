91801ee352a85182570358c1db5ac25ed84b070b
cinderallaman1 <ahmaddddddddddkhan@gmail.com>
2023-02-22 00:30:33 +0500

    Largest value in each level of BST

--- C++/largevaluesineachlevelbst.cpp
+++ C++/largevaluesineachlevelbst.cpp
@@ -1,31 +1,33 @@
-class Solution{
-    public:
-    vector<int>largestvalueineachlevel(TreeNode*root){
-        if(root == NULL)
-            return {};
-        vector<int>max_vals;
-        int size;
-        int max;
-        TreeNode*temp;
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty()){
-            size = q.size();
-            max = INT_MIN;
-            while(size--){
-                temp = q.front();
-                q.pop();
-                max = max(max, temp->val);
-                if(temp->left)
-                    q.push(temp->left);
-                if(temp->right){
-                    q.push(temp->right);
-                }
-            max_vals.push_back(max);
-            }
-        }
-        return max_vals;
+/*Finding largest value in each level of a BST*/
 
-    }
+// class Solution{
+//     public:
+//     vector<int>largestvalueineachlevel(TreeNode*root){
+//         if(root == NULL)
+//             return {};
+//         vector<int>max_vals;
+//         int size;
+//         int max;
+//         TreeNode*temp;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         while(!q.empty()){
+//             size = q.size();
+//             max = INT_MIN;
+//             while(size--){
+//                 temp = q.front();
+//                 q.pop();
+//                 max = max(max, temp->val);
+//                 if(temp->left)
+//                     q.push(temp->left);
+//                 if(temp->right){
+//                     q.push(temp->right);
+//                 }
+//             max_vals.push_back(max);
+//             }
+//         }
+//         return max_vals;
 
-};
+//     }
+
+// };
